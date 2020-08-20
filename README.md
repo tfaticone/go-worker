8262c0462bffa3fc454e29e8ec5a723b6be52fa1
Thomas Faticone <tfaticone@yahoo.com>
2020-08-19 19:41:50 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# go-worker
-Golang worker used for process heavy activities
+# Go Worker Pattern
+
+## Description
+Golang worker used for process heavy activities. 
+This is strongly based off of the article [here](http://marcio.io/2015/07/handling-1-million-requests-per-minute-with-golang/)
+by Marcio Castilho over at Malwarebytes. I recommend reading
+the article to understand the inner mechanics of the worker pattern 
+used here. With that, this project sets out to "finish off" the
+pattern that was outlined in the article in a reusable and manageble way
+, provide some working examples for usage, and give a few strategies for testing.
+
+## Usage
+
+## Testing
+#### Methodology 
+
+#### Testing a singular job
+
+#### Testing the logic around the job
---
+++ pkg/worker/worker.go
@@ -0,0 +1,58 @@
+package worker
+
+import "log"
+
+type JobInterface interface {
+	GetPayload() interface{}
+	PanicOnFailure() bool
+	Handler() error
+}
+
+// A buffered channel that we can send work requests on.
+var JobQueue chan JobInterface
+
+// Worker represents the worker that executes the job
+type Worker struct {
+	WorkerPool  chan chan JobInterface
+	JobChannel  chan JobInterface
+	quit    	chan bool
+}
+
+func NewWorker(workerPool chan chan JobInterface) Worker {
+	return Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan JobInterface),
+		quit:       make(chan bool)}
+}
+
+// Start method starts the run loop for the worker, listening for a quit channel in
+// case we need to stop it
+func (w Worker) Start() {
+	go func() {
+		for {
+			// register the current worker into the worker queue.
+			w.WorkerPool <- w.JobChannel
+
+			select {
+			case job := <-w.JobChannel:
+				// we have received a work request.
+				err := job.Handler()
+				if err != nil && job.PanicOnFailure() {
+					panic(err)
+				} else if err != nil && !job.PanicOnFailure() {
+					log.Panic("Error occurred during handle: ", err.Error())
+				}
+			case <-w.quit:
+				// we have received a signal to stop
+				return
+			}
+		}
+	}()
+}
+
+// Stop signals the worker to stop listening for work requests.
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
